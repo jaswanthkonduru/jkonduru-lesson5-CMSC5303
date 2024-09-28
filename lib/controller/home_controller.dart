2da75aeaf7919c54312bb1d8f4c2da737e8377f7
jaswanthkonduru <jaswanthkonduru27@gmail.com>
2024-09-27 19:00:29 -0500

    intial

---
+++ lib/controller/home_controller.dart
@@ -0,0 +1,24 @@
+import 'package:lesson5/model/game_model.dart';
+import 'package:lesson5/view/home_screen.dart';
+
+class HomeController {
+  HomeState state;
+  HomeController(this.state);
+
+  void onPressedNewGame() {
+    state.callSetState(() {
+      state.model.restart();
+    });
+  }
+
+  void onPressedBoard(int index) {
+    state.callSetState(() {
+      state.model.board[index] = state.model.turn;
+      state.model.changeTurns();
+      state.model.setWinner();
+      if (state.model.winner != null) {
+        state.model.state = GameState.over;
+      }
+    });
+  }
+}
